956bb8218901f9e079746932e732a64047f0a099
michael <michaelglenn88@yahoo.com>
2022-05-31 17:04:40 -0400

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isCart, setCart] = useState(false);
+
+  function click() {
+    setCart((isCart) => !isCart);
+  }
+  const itemCart = isCart ? "in-cart" : "";
+
+  const textButton = isCart ? "Remove From Cart" : "Add to Cart";
+  const style = isCart ? "purple" : "";
   return (
-    <li className="">
+    <li className={itemCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={click} className="add" style={{ backgroundColor: style }}>
+        {textButton}
+      </button>
     </li>
   );
 }
